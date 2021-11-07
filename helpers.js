f4ece55e8df79a824e871cc7d59e9b3690c05351
Malte Hoch <mhoch94@gmail.com>
2021-11-07 12:26:33 +0100

    1.1

--- helpers.js
+++ helpers.js
@@ -21,6 +21,36 @@ export function useStateCallback(initialState) {
     return [state, setStateCallback];
   }
 
+/* Usage:
+
+import {useStateCallback as state} from './helpers.js';
+import './App.css';
+
+export default function App() {
+
+  const [count, setCount] = state(0);
+
+  const handleClick = () => {
+    setCount(count + 1, count => {
+      if (count > 5) {
+        console.log('Over 5');
+      }
+      console.log(count);
+    });
+  };
+
+  return (
+    <div className="App">
+      Count: {count}
+      <button onClick={handleClick}>Increase</button>
+    </div>
+  );
+}
+
+###############################################################################
+
+*/
+
 export function useToggle(initialVal = false) {
     // call useState, "reserve piece of state"
     const [state, setState] = useState(initialVal);
@@ -30,6 +60,27 @@ export function useToggle(initialVal = false) {
     // return piece of state AND a function to toggle it
     return [state, toggle];
   }
+
+/* Usage: 
+
+import useToggle from "./hooks/useToggle";
+
+export default function Toggler() {
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isHeartbroken, toggleIsHeartbroken] = useToggle(false);
+  const [isBanana, toggleIsBanana] = useToggle(true);
+  return (
+    <div>
+      <h1 onClick={toggleIsHappy}>{isHappy ? "😄" : "😢"}</h1>
+      <h1 onClick={toggleIsHeartbroken}>{isHeartbroken ? "💔" : "❤️"}</h1>
+      <h1 onClick={toggleIsBanana}>{isBanana ? "🍌" : "🍏"}</h1>
+    </div>
+  );
+}
+
+###############################################################################
+
+*/
 
 export function useFormInput(initialVal) {
     const [value, setValue] = useState(initialVal);
@@ -41,3 +92,25 @@ export function useFormInput(initialVal) {
     };
     return [value, handleChange, reset];
 };
+
+/* Usage:
+
+export default function SimpleFormInputHook() {
+  const [email, updateEmail, resetEmail] = useInputState("");
+  const [password, updatePassword, resetPassword] = useInputState("");
+  return (
+    <div>
+      <h1>
+        Email is: {email} & Password is: {password}
+      </h1>
+      <input type='text' value={email} onChange={updateEmail} />
+      <input type='text' value={password} onChange={updatePassword} />
+      <button onClick={resetEmail}>Reset Email</button>
+      <button onClick={resetPassword}>Reset Password</button>
+    </div>
+  );
+}
+
+###############################################################################
+
+*/
